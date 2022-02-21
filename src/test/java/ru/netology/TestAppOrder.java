39a927f8058fbef24daedd96796b6b1ea7b4c4bc
STANISLAV LITVINOV <91020862+Stanislav-Litvinov@users.noreply.github.com>
2022-02-21 16:49:35 +0300

    added test

--- src/test/java/ru/netology/TestAppOrder.java
+++ src/test/java/ru/netology/TestAppOrder.java
@@ -12,11 +12,12 @@ public class TestAppOrder {
     @Test
     void shouldSubmitRequest() {
         open("http://localhost:9999");
-        SelenideElement form = $("[data-test-id=callback-form]");
-        form.$("[data-test-id=name] input").setValue("Василий");
-        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        SelenideElement form = $("#root");
+        form.$("[data-test-id=name] input").setValue("Коржов Василий");
+        form.$("[data-test-id=phone] input.input__control").setValue("+79633414412");
         form.$("[data-test-id=agreement]").click();
-        form.$("[data-test-id=submit]").click();
-        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+        form.$("[type=\"button\"]").click();
+        $(".Success_successBlock__2L3Cw").shouldHave(exactText("  Ваша заявка успешно отправлена!" +
+                " Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
